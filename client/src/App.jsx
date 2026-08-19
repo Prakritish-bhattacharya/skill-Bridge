09c2b290a176d33b23fffd0bcb9ae447f9df5a99
prakritish <bhattacharyaprakritish@gmail.com>
2026-08-19 08:59:54 +0530

    refining

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useAuth } from "./components/AuthProvider";
-import Dashboard from "./components/Dashboard";
+
+
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-950">
-      <Dashboard/>
+    <div >
+      <h1>Home page</h1>
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,42 +4,13 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
-import AuthProvider from "./components/AuthProvider.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import PublicOnlyRoute from "./components/PublicOnlyRoute.jsx";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <App />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <PublicOnlyRoute>
-        <Login />
-      </PublicOnlyRoute>
-    ),
-  },
-  {
-    path: "/register",
-    element: (
-      <PublicOnlyRoute>
-        <Register />
-      </PublicOnlyRoute>
-    ),
-  },
-]);
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={appRouter} />
-    </AuthProvider>
+   
+      <App/>
+   
   </StrictMode>,
 );
